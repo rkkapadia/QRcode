4faa343067a8ca21e1e69d48dc5e5af3b84a1d6e
rajkapadia <rkkapadi@ualberta.ca>
2018-12-01 13:12:41 -0700

    initial commint

---
+++ app/src/main/java/com/rkkapadi/qrcode/MainActivity.java
@@ -0,0 +1,68 @@
+package com.rkkapadi.qrcode;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = "GenerateQRCode";
+    ImageView qrImage;
+    Button generate;
+    String inputValue = "rajkapadia";
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrImage = (ImageView) findViewById(R.id.qrImageView);
+        generate = (Button) findViewById(R.id.generateButton);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (inputValue.length() > 0) {
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+                    Point point = new Point();
+                    display.getSize(point);
+                    int width = point.x;
+                    int height = point.y;
+                    int smallerDimension = width < height ? width : height;
+                    smallerDimension = smallerDimension * 3 / 4;
+
+                    qrgEncoder = new QRGEncoder(
+                            inputValue, null,
+                            QRGContents.Type.TEXT,
+                            smallerDimension);
+                    try {
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrImage.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        Log.v(TAG, e.toString());
+                    }
+                } else {
+                }
+            }
+        });
+
+    }
+}
